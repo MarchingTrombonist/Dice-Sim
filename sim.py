63653681901cdb49531ac409aa70e37e591743da
MarchingTrombonist <marchingtrombonist@gmail.com>
2024-04-06 10:07:45 -0400

    uploading rolls

--- sim.py
+++ sim.py
@@ -7,16 +7,16 @@ import math
 pd.set_option("display.max_rows", None)
 pd.set_option("display.max_colwidth", 100)
 
-dice = [4, 6, 8, 10, 12, 20]
-sides = [4, 6, 8, 10, 12, 20]
-# rolls = 10**6
+dice = [6, 8, 12]
+sides = [6, 8, 12]
+rolls = 10**6
 # rolls = 1
 
 trials = []
 
 for die in dice:
     for side in sides:
-        rolls = math.floor(10 ** ((((die * side) - 16) / 384) * 3 + 5))
+        # rolls = math.floor(10 ** ((((die * side) - 16) / 384) * 3 + 5))
         # trials = []
         print("Rolling {2} sets of {0}d{1}".format(die, side, rolls))
         Roll = roller2(die, side, rolls)
@@ -34,7 +34,7 @@ df = df.fillna(0)
 # df = df[["6d6", "6d8", "6d12", "8d6", "8d8", "8d12"]]
 df = df.sort_values(df.columns[0], axis=0, ascending=False)
 # df = df.reindex(["High", "2", "22", "3", "Straight", "32", "4", "5"])
-# df = (df.div(df.sum(axis=0), axis=1) * 100).round(2)
+df = (df.div(df.sum(axis=0), axis=1) * 100).round(2)
 
 # df.loc[:, "Percentage"] = (df.div(df.sum(axis=0), axis=1) * 100).round(2)
 
